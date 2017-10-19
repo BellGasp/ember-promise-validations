cf52d3c9f9034a4f0df0d49e228fe7736b4369ea
Bobby Mousseau <bobby_mousseau@hotmail.com>
2017-10-19 10:34:24 -0400

    Issue-3: Validation as block component
    - Adds possibility to use validation-summary as a block component;
    - Makes validation-summary hidden if validations are empty;

--- addon/components/validation-summary.js
+++ addon/components/validation-summary.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 import layout from '../templates/components/validation-summary';
+import { isEmpty } from '@ember/utils';
+import { computed } from '@ember/object';
 
 export default Ember.Component.extend({
   layout,
-  tagName: 'ul'
+  tagName: 'ul',
+  classNameBindings: ['isHidden:hidden'],
+
+  validations: null,
+
+  isHidden: computed('validations.[]', function() {
+    let validations = this.get('validations');
+    return isEmpty(validations);
+  })
 });
--- tests/integration/components/validation-summary-test.js
+++ tests/integration/components/validation-summary-test.js
@@ -38,3 +38,48 @@ test('it renders validation errors', function(assert) {
     assert.equal(this.$('li:eq(1)').text().trim(), 'Some other error message');
   });
 });
+
+test('it renders validation errors as block', function(assert) {
+  let promiseAwareObject = ObjectProxy.extend(PromiseProxyMixin);
+
+  this.set('object', EmberObject.create({
+    validationErrors: A([{
+      validation: 'test',
+      error: promiseAwareObject.create({
+        isFulfilled: true,
+        promise: resolve('Some error message'),
+        content: 'Some error message'
+      })
+    }, {
+      validation: 'otherTest',
+      error: promiseAwareObject.create({
+        isFulfilled: true,
+        promise: resolve('Some other error message'),
+        content: 'Some other error message'
+      })
+    }])
+  }));
+
+  this.render(hbs`
+    {{#validation-summary validations=object.validationErrors as |message|}}
+      error: {{message}}
+    {{/validation-summary}}`);
+  wait().then(() => {
+    assert.equal(this.$('li:eq(0)').text().trim(), 'error: Some error message');
+    assert.equal(this.$('li:eq(1)').text().trim(), 'error: Some other error message');
+  });
+});
+
+test('it renders hidden when no validations', function(assert) {
+  this.set('object', EmberObject.create({
+    validationErrors: A([])
+  }));
+
+  this.render(hbs`
+    {{#validation-summary validations=object.validationErrors class="alert alert-danger" as |message|}}
+      error: {{message}}
+    {{/validation-summary}}`);
+  wait().then(() => {
+    assert.equal(this.$('ul').hasClass('hidden'), true);
+  });
+});
