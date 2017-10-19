e4d19cc41832595d2b8ea5ae3a7aa04921e42d5a
Bobby Mousseau <bobby_mousseau@hotmail.com>
2017-10-19 10:44:46 -0400

    Issue-1: Fixes blueprint
    - Fixes validator-test blueprint having 'service' in it's definition instead of validator;

--- blueprints/validator-test/files/tests/unit/validators/__name__-test.js
+++ blueprints/validator-test/files/tests/unit/validators/__name__-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('validator:<%= dasherizedModuleName %>', 'Unit | Service | <%= classifiedModuleName %>', {
+moduleFor('validator:<%= dasherizedModuleName %>', 'Unit | Validator | <%= classifiedModuleName %>', {
   // Specify the other units that are required for this test.
   needs: []
 });
